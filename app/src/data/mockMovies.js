9bde9024c2112439031a654258847b4c54d97a11
Natalia Novikova <novikova.nm@phystech.edu>
2025-04-20 03:13:30 +0300

    Тестовые данные (данные о фильмах) созданы, экспортируются правильно)

---
+++ app/src/data/mockMovies.js
@@ -0,0 +1,16 @@
+
+export const mockMovies = [
+    { id: 1, title: 'Матрица', posterUrl: 'app/src/images/Matrix.png', genre: 'Боевик', duration: 136, isFavorite: true },
+    { id: 2, title: 'Безумный Макс', posterUrl: 'app/src/images/Mad_Max.png', genre: 'Боевик', duration: 88, isFavorite: false },
+    { id: 3, title: 'Джентельмены', posterUrl: 'app/src/images/Gentlemen.png', genre: 'Драма', duration: 113, isFavorite: false },
+    { id: 4, title: 'Отступники', posterUrl: 'app/src/images/Renegades.png', genre: 'Триллер', duration: 151, isFavorite: false },
+    { id: 5, title: 'Гладиатор', posterUrl: 'app/src/images/Gladiator.png', genre: 'Боевик', duration: 155, isFavorite: false },
+    { id: 6, title: 'Однажды в Голливиде', posterUrl: 'app/src/images/Once_in_HW.png', genre: 'Драма', duration: 161, isFavorite: true },
+    { id: 7, title: 'Предложение', posterUrl: 'app/src/images/Purpose.png', genre: 'Комедия', duration: 108, isFavorite: false },
+    { id: 8, title: 'Малышка на миллион', posterUrl: 'app/src/images/Million_baby.png', genre: 'Драма', duration: 132, isFavorite: true },
+    { id: 9, title: 'Ларри Краун', posterUrl: 'app/src/images/Larry_Crown.png', genre: 'Комедия', duration: 98, isFavorite: false },
+    
+];
+
+
+export const genres = ['Боевик', 'Триллер', 'Комедия', 'Драма'];
